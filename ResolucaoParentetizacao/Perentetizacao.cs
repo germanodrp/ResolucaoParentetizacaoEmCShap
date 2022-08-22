c3a7185b291447bb407d526d8a008ca38066f332
Germano Pereira <germano.pereira@bit.com.br>
2022-08-22 11:21:32 -0300

    Concluido

---
+++ ResolucaoParentetizacao/Perentetizacao.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResolucaoParentetizacao
+{
+    public class Parentetizacao
+    {
+        int open = 0;
+        string parentetizacao = "()";
+
+        public bool verificarParentetizacao()
+        {
+            
+            for (var i = 0; i < parentetizacao.Length; i++)
+            {
+                if (parentetizacao[i] == '(')
+                {                
+                    open++;
+                }
+                else
+                {
+                    open--;
+                }
+               
+            }
+            if (open == 0)
+            {
+                return true;
+            }
+            return false;
+
+        }
+
+    }
+}
+
---
+++ ResolucaoParentetizacao/Program.cs
@@ -0,0 +1,14 @@
+﻿using ResolucaoParentetizacao;
+
+var teste = new Parentetizacao();
+
+
+if (teste.verificarParentetizacao())
+{
+    Console.WriteLine("Deu certo");
+
+}
+else
+{
+    Console.WriteLine("Errado");
+}
